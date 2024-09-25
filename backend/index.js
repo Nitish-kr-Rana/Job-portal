9472199e76232e0fadb4be7ae365a7d3b7615473
Nitish Kumar <nitishkumarwrs01@gmail.com>
2024-09-26 00:24:22 +0530

    first.jj

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
+dotenv.config({});
+
+
+const app = express();
+connectDB();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:process.env.ALLOWED_ORIGIN,
+    credentials:true
+}
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+
+// api's
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server running at port ${PORT}`);
+})
+export default app;
---
+++ frontend/src/utils/constant.js
@@ -0,0 +1,5 @@
+const SERVER_URL=import.meta.env.VITE_USER_API_END_POINT
+export const USER_API_END_POINT=`${SERVER_URL}/api/v1/user`;
+export const JOB_API_END_POINT=`${SERVER_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT=`${SERVER_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT=`${SERVER_URL}/api/v1/company`;
